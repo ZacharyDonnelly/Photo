a47f999d1eea9fd6568b22c0cc989e50215f2f14
Zach Donnelly <zachary.donnelly@walmart.com>
2024-09-08 22:04:15 -0400

    feat: project skeleton

---
+++ app/ui/components/layout/header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <div className='border-b-2 w-96  border-black'>
+      <h1 className='text-red-700'>Hello der</h1>
+    </div>
+  );
+};
+
+export default Header;
